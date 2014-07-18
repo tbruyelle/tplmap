07b057b3cb33db24cf0857a50e92e1a8154a8652
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2014-07-18 13:50:00 +0200

    Godoc

--- tplmap.go
+++ tplmap.go
@@ -1,9 +1,17 @@
+// Package tplmap is an helper for the native html/template package
 package tplmap
 
 import "html/template"
 
+// TemplateMap is simple map where the key is name of the template
+// and the value is the html/template
 type TemplateMap map[string]*template.Template
 
+// New returns a TemplateMap filled with the templates in parameters.
+// The templates have a common parent which is represented by the parent
+// parameter, which is a path to the template.
+// The template parameter contains the template name as the key (user
+// defined) and the path to the template as the value.
 func New(parent string, templates map[string]string) TemplateMap {
 	tplparent := template.Must(template.ParseFiles(parent))
 	t := make(TemplateMap, len(templates))
