b5777e70fcd6816f2593683f2ad483c76c1cc9ae
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2014-07-18 13:59:44 +0200

    Godoc

--- tplmap.go
+++ tplmap.go
@@ -9,9 +9,19 @@ type TemplateMap map[string]*template.Template
 
 // New returns a TemplateMap filled with the templates in parameters.
 // The templates have a common parent which is represented by the parent
-// parameter, which is a path to the template.
-// The template parameter contains the template name as the key (user
-// defined) and the path to the template as the value.
+// parameter, which is a path to a common template.
+// The templates parameter contains multiple templates where the template
+// name is the key (user defined) and the path to the template as the value.
+//
+// Example:
+//   // Create the TemplateMap
+//   templates = tplmap.New("views/layout.html", map[string]string{
+//		"main":  "views/main.html",
+//		"login": "views/login.html",
+//	})
+//
+//   // Use it
+//   templates["main"].Execute(w, data)
 func New(parent string, templates map[string]string) TemplateMap {
 	tplparent := template.Must(template.ParseFiles(parent))
 	t := make(TemplateMap, len(templates))
